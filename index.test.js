716cb59919ee7b708f9051c6b11a9072723e7db8
Andrew <106277595+andrewozo@users.noreply.github.com>
2023-11-20 16:07:40 -0500

    part 1 GET (#1)

--- index.test.js
+++ index.test.js
@@ -1,22 +1,19 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
 const seedMusician = require("./seedData");
 
+describe("./musicians endpoint", () => {
+  // Write your tests here
+  test("get all musicians", async () => {
+    const response = await request(app).get("/musicians");
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+    expect(response.body[0].name).toBe("Mick Jagger");
+  });
+});
